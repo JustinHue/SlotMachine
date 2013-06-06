a6e4a8c6c22d79b79d675d9fe8fe68e635d8173f
Justin Hellsten <justinhellsten@gmail.com>
2013-06-06 11:06:43 -0400

    first commit

---
+++ slot_machine/utility.py
@@ -0,0 +1,24 @@
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#
+# Source File: utility.py
+# Author Name: Justin Hellsten
+# Last Modified By: Justin Hellsten
+# Last Modified Date: June 6, 2013
+#
+# Program Description: 
+#
+#        This file holds utility functions that will help build game or application projects in Python. 
+#
+# Version 0.1:
+#
+#        - Configuration file read/write
+#        - System configuration file override
+#        - initialize from system configuration file
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# This util function initializes pygame para
+def util_init():
+    
+def util_override_configuration(file):
+    
+def util_set_configuration_file(file):
