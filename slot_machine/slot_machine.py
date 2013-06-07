bfdc346d412951f4b5e7aa5379bc09d366907c09
Justin Hellsten <justinhellsten@gmail.com>
2013-06-07 11:54:42 -0400

    Version 0.5
    # Version 0.5
    #
    #    - Added meta data on ButtonEventHandler for quick referencing on
    the button
    #    - Added state listeners on the button object (e.g hover, press,
    release)
    #    - Changed run method on ButtonEventHandler to execute event
    listeners
    #    - Added static class ExEventHandler (a storage place for our event
    handler functions)
    #        -> quit_button_hover_listener(),
    quit_button_release_listener(), quit_button_press_listener()
    #        -> reset_button_hover_listener(),
    reset_button_release_listener(), reset_button_press_listener()
    #    - Made the buttons scale and work with even listeners

--- slot_machine/slot_machine.py
+++ slot_machine/slot_machine.py
@@ -9,17 +9,16 @@
 #
 #        This is a slot machine game.
 #
-# Version 0.4
+# Version 0.5
 #
-#    - Updated Render class
-#        -> Updated button render function
-#        -> Updated slot machine render function
-#        -> Added draw component function (checks class name and draws corresponding render function)
+#    - Added meta data on ButtonEventHandler for quick referencing on the button
+#    - Added state listeners on the button object (e.g hover, press, release)
+#    - Changed run method on ButtonEventHandler to execute event listeners 
+#    - Added static class ExEventHandler (a storage place for our event handler functions)
+#        -> quit_button_hover_listener(), quit_button_release_listener(), quit_button_press_listener()
+#        -> reset_button_hover_listener(), reset_button_release_listener(), reset_button_press_listener()
+#    - Made the buttons scale and work with even listeners
 #
-#    - Added threading for button even handling
-#    - Added Event Handler object
-#        -> run method implemented
-# 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -28,25 +27,99 @@ import pygame
 import threading
 import utility 
 
+
+class Resource:
+    normalReel = None
+    blurReel = None
+    slotMachine = None
+    
+
+    redButton = None
+    blueButton = None
+    greenButton = None
+    purpleButton = None
+    
+    
+class ExEventHandler:
+    @staticmethod
+    def button_hover_listener(button, hover_state, x, y):
+        if hover_state:
+            button.changeImage(Resource.purpleButton)
+            pygame.mouse.set_cursor(*pygame.cursors.diamond)
+             
+        else:
+            button.changeImage(Resource.redButton)
+            pygame.mouse.set_cursor(*pygame.cursors.arrow)
+        
+    @staticmethod
+    def button_release_listener(button, m1, m2, m3, x, y):
+        button.changeWidth(60)
+        button.changeHeight(60)        
+        print 'release'
+       
+    @staticmethod
+    def button_press_listener(button, m1, m2, m3, x, y):
+        button.changeWidth(55)
+        button.changeHeight(55)
+        print 'press'
+        
+    @staticmethod
+    def reset_button_hover_listener(button, hover_state, x, y):
+        if hover_state:
+            button.changeImage(Resource.purpleButton)
+            pygame.mouse.set_cursor(*pygame.cursors.diamond)
+             
+        else:
+            button.changeImage(Resource.redButton)
+            pygame.mouse.set_cursor(*pygame.cursors.arrow)
+        
+    @staticmethod
+    def reset_button_release_listener(button, m1, m2, m3, x, y):
+        button.changeWidth(60)
+        button.changeHeight(60)        
+        print 'release'
+       
+    @staticmethod
+    def reset_button_press_listener(button, m1, m2, m3, x, y):
+        button.changeWidth(55)
+        button.changeHeight(55)
+        print 'press'
+                
+         
 class ButtonEventHandler(threading.Thread):
     
     def __init__(self, buttonRef):
+        threading.Thread.__init__(self)
+
         self.__buttonRef = buttonRef
         self.__isRunning = True
+        # Add meta data for quick referencing on the button for better performance.        
         self.__meta__buttonX = self.__buttonRef.getX()
-        
-                    mousePos = pygame.mouse.get_pos()
-            buttonX = self.__buttonRef.getX()
-            buttonY = self.__buttonRef.getY()
-            buttonWidth = self.__buttonRef.getWidth()
-            buttonHeight = self.__buttonRef.getHeight()
+        self.__meta__buttonY = self.__buttonRef.getY()
+        self.__meta__buttonWidth = self.__buttonRef.getWidth()
+        self.__meta__buttonHeight = self.__buttonRef.getHeight()
+
         
     def run(self):
         while self.__isRunning:
             pygame.time.wait(1)
 
-            if mousePos[0] >= buttonX && mousePos[0] <= buttonX + buttonWidth &&
-               mousePos[0] >= buttonX && mousePos[0] <= buttonX + buttonWidth
+            mousePos = pygame.mouse.get_pos()
+            
+            if mousePos[0] >= self.__meta__buttonX and \
+            mousePos[0] <= self.__meta__buttonX + self.__meta__buttonWidth and \
+            mousePos[1] >= self.__meta__buttonY and \
+            mousePos[1] <= self.__meta__buttonY + self.__meta__buttonHeight:
+                self.__buttonRef.getOnHoverListener()(self.__buttonRef, True, mousePos[0], mousePos[1])
+                mousePress = pygame.mouse.get_pressed()
+                if mousePress[0] or mousePress[1] or mousePress[2]:
+                    self.__buttonRef.getOnPressListener()(self.__buttonRef, mousePress[0], mousePress[1],
+                                                        mousePress[2], mousePos[0], mousePos[1])
+                else:
+                    self.__buttonRef.getOnReleaseListener()(self.__buttonRef, mousePress[0], mousePress[1],
+                                                        mousePress[2], mousePos[0], mousePos[1])
+            else:
+                self.__buttonRef.getOnHoverListener()(self.__buttonRef, False, mousePos[0], mousePos[1])
     
 
 class Button:
@@ -58,7 +131,15 @@ class Button:
         self.__text = text
         self.__width = width
         self.__height = height
-        self.__eventhandler = ButtonEventHandler()
+        self.__eventhandler = ButtonEventHandler(self)
+        # states
+        #self.__hover = False
+        #self.__pressed = False
+        # Event listeners
+        self.__hoverEventListener = None
+        self.__pressEventListener = None
+        self.__releaseEventListener = None
+        
         self.__eventhandler.start()
 
     def getX(self):
@@ -82,7 +163,7 @@ class Button:
         return self.__imgRef
     
     def changeImage(self, imgRef):
-        self.____imgRef = imgRef
+        self.__imgRef = imgRef
         
     def getText(self):
         return self.__text
@@ -99,17 +180,42 @@ class Button:
     def getHeight(self):
         return self.__height
     
-    @staticmethod
-    def isPressed
+    def changeWidth(self, width):
+        self.__width = width
+        
+    def changeHeight(self, height):
+        self.__height = height
+        
+    def setOnHoverListener(self, eventListener):
+        self.__hoverEventListener = eventListener
+        
+    def setOnPressListener(self, eventListener):
+        self.__pressEventListener = eventListener
+        
+    def setOnReleaseListener(self, eventListener):
+        self.__releaseEventListener = eventListener
+        
+    def getOnHoverListener(self):
+        return self.__hoverEventListener
+        
+    def getOnPressListener(self):
+        return self.__pressEventListener
+    
+    def getOnReleaseListener(self):
+        return self.__releaseEventListener
+
+        
 class Render:
     @staticmethod
     def draw_button(destination, button):
-        destination.blit(button.getImage(), (button.getX(), button.getY()))
+        scaleImage = pygame.transform.scale(button.getImage(), (button.getWidth(),button.getHeight()))
+        destination.blit(scaleImage, (button.getX(), button.getY()))
         myfont = pygame.font.SysFont(None, 24)
         label = myfont.render(button.getText(), True, (0, 0, 0))
         labeldims = myfont.size(button.getText())
         destination.blit(label, (button.getX() + button.getWidth() / 2 - labeldims[0] / 2,
                                  button.getY() + button.getHeight() / 2 - labeldims[1] / 2))
+         
     
     @staticmethod
     def draw_component(destination, component):
@@ -127,17 +233,7 @@ class Render:
             
     
     
-class Resource:
-    normalReel = None
-    blurReel = None
-    slotMachine = None
-    
 
-    redButton = None
-    blueButton = None
-    greenButton = None
-    purpleButton = None
-    
     
 class SlotMachine:
     def __init__(self, imgRef):
@@ -157,7 +253,7 @@ class SlotMachine:
         return self.__components[component]
     
     def getComponentAtIndex(self, index):
-        return self.__components.index(index)
+        return self.__components[index]
     
     def getComponents(self):
         return self.__components
@@ -192,7 +288,12 @@ def init():
     
     # Initialize the Slot Machine
     slotmachine = SlotMachine(Resource.slotMachine)
-    slotmachine.addComponent(Button(630, 50, 60, 60, 'Quit', Resource.redButton))
+    quitButton = Button(630, 50, 60, 60, 'Quit', Resource.redButton)
+    quitButton.setOnHoverListener(ExEventHandler.quit_button_hover_listener)
+    quitButton.setOnPressListener(ExEventHandler.quit_button_press_listener)
+    quitButton.setOnReleaseListener(ExEventHandler.quit_button_release_listener)
+    slotmachine.addComponent(quitButton)
+    
     slotmachine.addComponent(Button(731, 52, 60, 60, 'Reset', Resource.redButton))
     slotmachine.addComponent(Button(300, 400, 60, 60, 'Spin', Resource.greenButton))
     
@@ -210,7 +311,6 @@ def update():
 def render(screen, slotmachine):
     Render.draw_slotmachine(screen, slotmachine)
 
-   
    
 def main():
     
